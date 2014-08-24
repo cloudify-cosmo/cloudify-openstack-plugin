ed2f3734800d2f9466d3efc8b6cc64b7f901cc6c
Ran Ziv <ran@gigaspaces.com>
2014-08-24 18:19:18 +0300

    CFY-1159 removed __author__ field

--- neutron_plugin/security_group_rule.py
+++ neutron_plugin/security_group_rule.py
@@ -13,8 +13,6 @@
 #  * See the License for the specific language governing permissions and
 #  * limitations under the License.
 
-__author__ = 'ran'
-
 import re
 
 import neutronclient.common.exceptions as neutron_exceptions

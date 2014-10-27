edc6f8e4306cef098fba5054f44bb447889f4aeb
opencm <limor@gigaspaces.com>
2014-10-27 16:06:17 +0200

    Bump version to 1.1rc1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup
 setup(
     zip_safe=True,
     name='cloudify-openstack-plugin',
-    version='1.1c1',
+    version='3.1rc1',
     author='idanmo',
     author_email='idan@gigaspaces.com',
     packages=[
@@ -33,7 +33,7 @@ setup(
     license='LICENSE',
     description='Cloudify plugin for OpenStack infrastructure.',
     install_requires=[
-        'cloudify-plugins-common==3.1c1',
+        'cloudify-plugins-common==3.1rc1',
         'python-novaclient==2.17.0',
         'python-keystoneclient==0.7.1',
         'python-neutronclient==2.3.4',

ab021ea19845f4f1c9b6984cc9bbcbf45b8bd29c
limor-gs <limor@gigaspaces.com>
2014-11-25 23:19:16 +0200

    Bump version to 3.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup
 setup(
     zip_safe=True,
     name='cloudify-openstack-plugin',
-    version='3.1ga',
+    version='3.1',
     author='idanmo',
     author_email='idan@gigaspaces.com',
     packages=[
@@ -33,7 +33,7 @@ setup(
     license='LICENSE',
     description='Cloudify plugin for OpenStack infrastructure.',
     install_requires=[
-        'cloudify-plugins-common==3.1ga',
+        'cloudify-plugins-common==3.1',
         'python-novaclient==2.17.0',
         'python-keystoneclient==0.7.1',
         'python-neutronclient==2.3.9',

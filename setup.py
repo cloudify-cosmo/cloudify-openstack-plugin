3fba8bd1edc5b8b57fc1b16f72f088fea4d6cfb3
Jessie Brami <jessie@gigaspaces.com>
2016-08-30 14:50:59 +0300

    Bumping version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 setup(
     zip_safe=True,
     name='cloudify-openstack-plugin',
-    version='1.4',
+    version='1.4.1.dev0',
     author='idanmo',
     author_email='idan@gigaspaces.com',
     packages=[

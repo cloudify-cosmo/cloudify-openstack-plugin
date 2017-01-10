33be96f4c25c8a56ef391226297dee82e9364d7e
Matt Wheeler <m@funkyhat.org>
2017-01-10 01:03:52 +0000

    Make plugin compatible with keystone v3 (#132)
    JIRAs: OPENSTACK-41 OPENSTACK-94 OPENSTACK-95

--- setup.py
+++ setup.py
@@ -34,12 +34,12 @@ setup(
     description='Cloudify plugin for OpenStack infrastructure.',
     install_requires=[
         'cloudify-plugins-common>=3.3.1',
-        'python-novaclient==2.26.0',
-        'python-keystoneclient==1.6.0',
-        'python-neutronclient==2.6.0',
-        'python-cinderclient==1.2.2',
+        'keystoneauth1==2.12.1',
+        'python-novaclient==7.0.0',
+        'python-keystoneclient==3.5.0',
+        'python-neutronclient==6.0.0',
+        'python-cinderclient==1.9.0',
         'python-glanceclient==2.5.0',
-        'keystoneauth1',
         'IPy==0.81'
     ]
 )

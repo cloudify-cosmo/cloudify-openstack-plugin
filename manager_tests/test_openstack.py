dea341d746bbe0ba49086f30856d6a26e527fd08
earthmant <trammell@cloudify.co>
2018-06-24 12:14:15 +0300

    update

--- manager_tests/test_openstack.py
+++ manager_tests/test_openstack.py
@@ -1,5 +1,6 @@
 # Built-in Imports
 import os
+from time import sleep
 
 # Cloudify Imports
 from ecosystem_tests import TestLocal, utils, labs
@@ -23,7 +24,7 @@ class TestOpenstack(TestLocal):
         }
         for secret_key, env_key in secrets.items():
             env_value = utils.get_secrets(secret_key)
-            os.environ[env_key] = env_value
+            os.environ[env_key] = env_value.value
 
     def check_openstack_resource(
             self, resource_id, resource_type, exists=True, command=None):
@@ -50,16 +51,18 @@ class TestOpenstack(TestLocal):
         self.assertEqual(0 if exists else 1, utils.execute_command(command))
 
     def check_resources_in_deployment_created(self, nodes, node_names):
+        for node in nodes:
+            if node['id'] not in node_names:
+                break
+            external_id = \
+                node['instances'][0]['runtime_properties'].get('external_id')
+            self.check_openstack_resource(external_id, node['node_type'])
+
+    def check_resources_in_deployment_deleted(self, nodes, node_names):
 
         for node in nodes:
-            self.assertTrue(node['id'] in node_names)
-            self.check_openstack_resource(
-                node['instances'][0]['runtime_properties']['external_id'],
-                node['node_type'])
-
-    def check_resources_in_deployment_deleted(self, nodes):
-
-        for node in nodes:
+            if node['id'] not in node_names:
+                break
             self.check_openstack_resource(
                 node['instances'][0]['runtime_properties']['external_id'],
                 node['node_type'], exists=False)
@@ -72,13 +75,15 @@ class TestOpenstack(TestLocal):
 
     def upload_plugins(self):
 
+        sleep(5)
         utils.update_plugin_yaml(
             os.environ['CIRCLE_SHA1'], 'openstack')
         workspace_path = os.path.join(
             os.path.abspath('workspace'),
             'build')
-        utils.upload_plugin(
-            utils.get_wagon_path(workspace_path))
+        sleep(5)
+        utils.upload_plugin(utils.get_wagon_path(workspace_path))
+        sleep(5)
         for plugin in self.plugins_to_upload:
             utils.upload_plugin(plugin[0], plugin[1])
 
@@ -99,10 +104,12 @@ class TestOpenstack(TestLocal):
             self.install_manager()
             self.addCleanup(self.uninstall_manager)
             self.password = 'admin'
+        os.environ['ECOSYSTEM_SESSION_PASSWORD'] = self.password
         os.environ['ECOSYSTEM_SESSION_MANAGER_IP'] = self.manager_ip
         utils.initialize_cfy_profile(
             '{0} -u admin -p {1} -t default_tenant'.format(
                 self.manager_ip, self.password))
+        sleep(300)
         self.upload_plugins()
         self.add_os_environment_variables()
 
@@ -133,12 +140,13 @@ class TestOpenstack(TestLocal):
                 blueprint_file_name=self.blueprint_file_name):
             raise Exception('Nodecellar install failed.')
         # Scale the deployment.
-        if utils.execute_scale('nc'):
+        if utils.execute_scale(
+                'nc', scalable_entity_name='nodejs_host_scale_group'):
             raise Exception('Nodecellar scale failed.')
         # Get list of nodes in the deployment.
         deployment_nodes = \
             utils.get_deployment_resources_by_node_type_substring(
-                'nc', 'cloudify.openstack.nodes')
+                'nc', 'cloudify')
         # Check that the nodes were created.
         self.check_resources_in_deployment_created(
             deployment_nodes, openstack_nodes)
@@ -148,7 +156,12 @@ class TestOpenstack(TestLocal):
         if utils.execute_uninstall('nc'):
             raise Exception('Nodecellar uninstall failed.')
         # Check that the nodes were deleted.
-        self.check_resources_in_deployment_deleted(deployment_nodes)
+        self.check_resources_in_deployment_deleted(
+            deployment_nodes,
+            openstack_nodes)
+        self.check_resources_in_deployment_deleted(
+            deployment_nodes,
+            monitored_nodes)
 
     def network_deployment(self):
         # Create a list of node templates to verify.
@@ -161,9 +174,16 @@ class TestOpenstack(TestLocal):
             'external_network'
         ]
         # Add delete deployment to cleanup even though we can trash the lab.
-        self.addCleanup(self.cleanup_network_blueprint)
+        # Deployment already exists. But installed with wrong plugin.
+        utils.execute_command(
+            'cfy executions start uninstall -d openstack-example-network')
+        utils.execute_command(
+            'cfy deployments delete openstack-example-network')
+        self.addCleanup(self.cleanup_network_deployment)
         # Create Deployment (Blueprint already uploaded.)
-        if utils.create_deployment('openstack-example-network'):
+        if utils.create_deployment(
+                'openstack-example-network',
+                inputs={'external_network_name': 'external_network'}):
             raise Exception(
                 'Deployment openstack-example-network failed.')
         # Install Deployment.
@@ -183,7 +203,9 @@ class TestOpenstack(TestLocal):
         if utils.execute_uninstall('openstack-example-network'):
             raise Exception('Uninstall openstack-example-network failed.')
         # Check that the nodes no longer exist.
-        self.check_resources_in_deployment_deleted(deployment_nodes)
+        self.check_resources_in_deployment_deleted(
+            deployment_nodes,
+            openstack_nodes)
 
     def deployments(self):
         self.network_deployment()

d3071f93f2427d47a4c7416d62b68c1e7e38e991
earthmant <trammell@cloudify.co>
2018-06-25 10:56:49 +0300

    adding more tests

--- manager_tests/test_openstack.py
+++ manager_tests/test_openstack.py
@@ -15,13 +15,15 @@ class TestOpenstack(TestLocal):
         pass
 
     def add_os_environment_variables(self):
+
         secrets = {
             'keystone_username': 'OS_USERNAME',
             'keystone_password': 'OS_PASSWORD',
             'keystone_tenant_name': 'OS_TENANT_NAME',
-            'keystone_url': 'OS_AUTH_URL',
-            'keystone_region': 'OS_REGION_NAME'
+            'keystone_region': 'OS_REGION_NAME',
+            'keystone_url': 'OS_AUTH_URL'
         }
+
         for secret_key, env_key in secrets.items():
             env_value = utils.get_secrets(secret_key)
             os.environ[env_key] = env_value.value
@@ -43,8 +45,10 @@ class TestOpenstack(TestLocal):
             command = 'neutron security-group-show {0}'.format(resource_id)
         elif 'FloatingIP' in resource_type:
             command = 'neutron floatingip-show {0}'.format(resource_id)
-        elif 'Server' in resource_type:
+        elif 'cloudify.openstack.nodes.Server' in resource_type:
             command = 'nova show {0}'.format(resource_id)
+        elif 'ServerGroup' in resource_type:
+            command = 'nova server-group-get {0}'.format(resource_id)
         else:
             raise Exception('Unsupported type {0} for {1}.'.format(
                 resource_type, resource_id))
@@ -52,6 +56,7 @@ class TestOpenstack(TestLocal):
 
     def check_resources_in_deployment_created(self, nodes, node_names):
         for node in nodes:
+            print 'Checking resource created: {0}'.format(node)
             if node['id'] not in node_names:
                 break
             external_id = \
@@ -59,7 +64,6 @@ class TestOpenstack(TestLocal):
             self.check_openstack_resource(external_id, node['node_type'])
 
     def check_resources_in_deployment_deleted(self, nodes, node_names):
-
         for node in nodes:
             if node['id'] not in node_names:
                 break
@@ -106,10 +110,10 @@ class TestOpenstack(TestLocal):
             self.password = 'admin'
         os.environ['ECOSYSTEM_SESSION_PASSWORD'] = self.password
         os.environ['ECOSYSTEM_SESSION_MANAGER_IP'] = self.manager_ip
+        sleep(300)
         utils.initialize_cfy_profile(
             '{0} -u admin -p {1} -t default_tenant'.format(
                 self.manager_ip, self.password))
-        sleep(300)
         self.upload_plugins()
         self.add_os_environment_variables()
 
@@ -118,6 +122,44 @@ class TestOpenstack(TestLocal):
         # Because its in the lab, don't care if it passes or fails.
         utils.execute_command(
             'cfy uninstall -p ignore_failure=true openstack-example-network')
+
+    def port_fixed_ip_deployment(self):
+        openstack_nodes = [
+            'port'
+        ]
+        utils.execute_command(
+            'cfy blueprints upload blueprints/port-fixed-ip.yaml '
+            '-b fixed-ip')
+        # Create Deployment (Blueprint already uploaded.)
+        if utils.create_deployment(
+                'fixed-ip', inputs={'fixed_ip': '192.168.113.250'}):
+            raise Exception(
+                'Deployment fixed-ip failed.')
+        # Install Deployment.
+        if utils.execute_install('fixed-ip'):
+            raise Exception(
+                'Install fixed-ip failed.')
+        # Get list of nodes in the deployment.
+        deployment_nodes = \
+            utils.get_deployment_resources_by_node_type_substring(
+                'fixed-ip', 'cloudify.openstack.nodes.Port')
+        # Check that we assigned the desired fixed IP.
+        fixed_ip_node = \
+            [node for node in deployment_nodes if node['id'] == 'port']
+        fixed_ip_instance = \
+            fixed_ip_node[0]['instances'][0]['runtime_properties']
+        if '192.168.113.250' != fixed_ip_instance['fixed_ip_address']:
+            raise Exception('IP Not Set.')
+        # Check that the nodes really exist.
+        self.check_resources_in_deployment_created(
+            deployment_nodes, openstack_nodes)
+        # Uninstall this deployment.
+        if utils.execute_uninstall('fixed-ip'):
+            raise Exception('Uninstall fixed-ip failed.')
+        # Check that the nodes no longer exist.
+        self.check_resources_in_deployment_deleted(
+            deployment_nodes,
+            openstack_nodes)
 
     def nodecellar_deployment(self):
         # Create a set of nodes to check.
@@ -197,8 +239,9 @@ class TestOpenstack(TestLocal):
         # Check that the nodes really exist.
         self.check_resources_in_deployment_created(
             deployment_nodes, openstack_nodes)
-        # Test another deployment that requires this deployment.
+        # Test another deployments that requires this deployment.
         self.nodecellar_deployment()
+        self.port_fixed_ip_deployment()
         # Uninstall this deployment.
         if utils.execute_uninstall('openstack-example-network'):
             raise Exception('Uninstall openstack-example-network failed.')
@@ -207,8 +250,48 @@ class TestOpenstack(TestLocal):
             deployment_nodes,
             openstack_nodes)
 
+    def server_groups_deployment(self):
+        # Create a list of node templates to verify.
+        openstack_nodes = [
+            'router',
+            'public_network',
+            'public_subnet',
+            'public_ip',
+            'server_groups_example_security_group',
+            'public_subnet_nic',
+            'example_key',
+            'server_group',
+            'server_group_member',
+        ]
+        utils.execute_command(
+            'cfy blueprints upload blueprints/server-groups.yaml '
+            '-b server-groups')
+        # Create Deployment (Blueprint already uploaded.)
+        if utils.create_deployment('server-groups'):
+            raise Exception(
+                'Deployment server-groups failed.')
+        # Install Deployment.
+        if utils.execute_install('server-groups'):
+            raise Exception(
+                'Install server-groups failed.')
+        # Get list of nodes in the deployment.
+        deployment_nodes = \
+            utils.get_deployment_resources_by_node_type_substring(
+                'server-groups', 'cloudify.openstack.nodes')
+        # Check that the nodes really exist.
+        self.check_resources_in_deployment_created(
+            deployment_nodes, openstack_nodes)
+        # Uninstall this deployment.
+        if utils.execute_uninstall('server-groups'):
+            raise Exception('Uninstall server-groups failed.')
+        # Check that the nodes no longer exist.
+        self.check_resources_in_deployment_deleted(
+            deployment_nodes,
+            openstack_nodes)
+
     def deployments(self):
         self.network_deployment()
+        self.server_groups_deployment()
 
     def test_run_tests(self):
         self.deployments()

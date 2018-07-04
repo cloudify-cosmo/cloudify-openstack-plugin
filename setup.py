3de89f0ab0fb8760aa4a5e8540d8b4660bc07c91
earthmant <trammell@cloudify.co>
2018-07-04 12:09:41 +0300

    bump version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 setup(
     zip_safe=True,
     name='cloudify-openstack-plugin',
-    version='2.9.2',
+    version='2.9.3',
     author='Cloudify',
     author_email='hello@cloudify.co',
     packages=[

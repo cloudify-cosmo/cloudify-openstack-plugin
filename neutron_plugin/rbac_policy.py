88c6dacee314a3f2237041d94b656122abdd7112
Krzysztof Bijakowski <krzysztof.bijakowski@cloudify.co>
2018-08-24 16:12:51 +0200

    RBAC policy support added

---
+++ neutron_plugin/rbac_policy.py
@@ -0,0 +1,191 @@
+#########
+# Copyright (c) 2018 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+#  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  * See the License for the specific language governing permissions and
+#  * limitations under the License.
+
+from cloudify import ctx
+from cloudify.decorators import operation
+from cloudify.exceptions import NonRecoverableError
+
+from openstack_plugin_common import (
+    with_neutron_client,
+    use_external_resource,
+    create_object_dict,
+    add_list_to_runtime_properties,
+    set_neutron_runtime_properties,
+    get_relationships_by_relationship_type,
+    delete_resource_and_runtime_properties,
+    COMMON_RUNTIME_PROPERTIES_KEYS,
+    OPENSTACK_ID_PROPERTY,
+    OPENSTACK_TYPE_PROPERTY
+)
+
+RBAC_POLICY_OPENSTACK_TYPE = 'rbac_policy'
+RBAC_POLICY_APPLIED_FOR_RELATIONSHIP_TYPE = \
+    'cloudify.openstack.rbac_policy_applied_to'
+
+
+def find_resource_to_apply_rbac_policy(ctx):
+    found_relationships = get_relationships_by_relationship_type(
+        ctx,
+        RBAC_POLICY_APPLIED_FOR_RELATIONSHIP_TYPE
+    )
+
+    if len(found_relationships) == 0:
+        ctx.logger.info(
+            'Resource for which RBAC policy may be applied '
+            'not found using {} relationship'
+            .format(RBAC_POLICY_APPLIED_FOR_RELATIONSHIP_TYPE)
+        )
+
+        return {}
+
+    if len(found_relationships) > 1:
+        raise NonRecoverableError(
+            'Multiple ({0}) resources for which RBAC policy may be applied '
+            'found using relationship {1}'
+            .format(
+                len(found_relationships),
+                RBAC_POLICY_APPLIED_FOR_RELATIONSHIP_TYPE
+            )
+        )
+
+    found_resource = found_relationships[0].target.instance
+    ctx.logger.info(
+        '{0} resource for which RBAC policy may be applied '
+        'found using {1} relationship)'
+        .format(found_resource, RBAC_POLICY_APPLIED_FOR_RELATIONSHIP_TYPE)
+    )
+
+    id = found_resource.runtime_properties.get(OPENSTACK_ID_PROPERTY, None)
+    type = found_resource.runtime_properties.get(
+        OPENSTACK_TYPE_PROPERTY,
+        None
+    )
+
+    if not id or not type:
+        ctx.logger.warn(
+            'Found using relationship resource has not defined either '
+            '"id" or "type" runtime_property. Skipping.'
+        )
+
+        return {}
+
+    return {
+        'object_type': type,
+        'object_id': id
+    }
+
+
+def validate_found_resource(input_dict, found_resource):
+    if found_resource:
+        for key in found_resource.keys():
+            if key in input_dict and input_dict.get(key):
+                raise NonRecoverableError(
+                    'Multiple definitions of resource for which '
+                    'RBAC policy should be applied. '
+                    'You specified it both using properties / operation '
+                    'inputs and relationship.'
+                )
+
+
+def create_rbac_policy_object_dict(ctx, args):
+    found_resource = find_resource_to_apply_rbac_policy(ctx)
+    validate_found_resource(
+        ctx.node.properties.get(RBAC_POLICY_OPENSTACK_TYPE, {}),
+        found_resource
+    )
+
+    validate_found_resource(
+        args.get(RBAC_POLICY_OPENSTACK_TYPE, {}),
+        found_resource
+    )
+
+    rbac_policy = create_object_dict(
+        ctx,
+        RBAC_POLICY_OPENSTACK_TYPE,
+        args,
+        found_resource
+    )
+
+    return rbac_policy
+
+
+@operation
+@with_neutron_client
+def create(neutron_client, args, **kwargs):
+    if use_external_resource(ctx, neutron_client, RBAC_POLICY_OPENSTACK_TYPE):
+        return
+
+    rbac_policy_raw = create_rbac_policy_object_dict(ctx, args)
+    ctx.logger.info('rbac_policy: {0}'.format(rbac_policy_raw))
+
+    rbac_policy = rbac_policy_raw.copy()
+    rbac_policy.pop('name', None)  # rbac_policy doesn't accept name parameter
+
+    rp = neutron_client.create_rbac_policy({
+        RBAC_POLICY_OPENSTACK_TYPE: rbac_policy
+    })[RBAC_POLICY_OPENSTACK_TYPE]
+    rp['name'] = None
+
+    set_neutron_runtime_properties(ctx, rp, RBAC_POLICY_OPENSTACK_TYPE)
+
+
+@operation
+@with_neutron_client
+def delete(neutron_client, **kwargs):
+    delete_resource_and_runtime_properties(
+        ctx,
+        neutron_client,
+        COMMON_RUNTIME_PROPERTIES_KEYS
+    )
+
+
+@operation
+@with_neutron_client
+def list_rbac_policies(neutron_client, args, **kwargs):
+    rbac_policies = neutron_client.list_rbac_policies(**args)
+    add_list_to_runtime_properties(
+        ctx,
+        RBAC_POLICY_OPENSTACK_TYPE,
+        rbac_policies.get('rbac_policies', [])
+    )
+
+
+@operation
+@with_neutron_client
+def find_and_delete(neutron_client, args, **kwargs):
+    reference_rbac_policy = create_rbac_policy_object_dict(ctx, args)
+    reference_rbac_policy.pop('name', None)
+    rbac_policies_list = neutron_client.list_rbac_policies() \
+        .get('rbac_policies', [])
+
+    for rbac_policy in rbac_policies_list:
+        if all(
+            item in rbac_policy.items()
+            for item
+            in reference_rbac_policy.items()
+        ):
+            id = rbac_policy['id']
+            ctx.logger.info(
+                'Found RBAC policy with ID: {0} - deleting ...'.format(id)
+            )
+
+            neutron_client.cosmo_delete_resource(
+                RBAC_POLICY_OPENSTACK_TYPE,
+                id
+            )
+
+            return
+
+    ctx.logger.warn('No suitable RBAC policy found')
---
+++ neutron_plugin/tests/test_rbac_policy.py
@@ -0,0 +1,530 @@
+import mock
+import unittest
+
+from cloudify.context import NODE_INSTANCE
+from cloudify.exceptions import NonRecoverableError
+from cloudify.mocks import (
+    MockContext,
+    MockNodeInstanceContext,
+    MockNodeContext,
+    MockRelationshipContext,
+    MockRelationshipSubjectContext
+)
+from openstack_plugin_common import (
+    OPENSTACK_ID_PROPERTY,
+    OPENSTACK_NAME_PROPERTY,
+    OPENSTACK_TYPE_PROPERTY
+)
+from neutron_plugin.rbac_policy import (
+    RBAC_POLICY_OPENSTACK_TYPE,
+    RBAC_POLICY_APPLIED_FOR_RELATIONSHIP_TYPE
+)
+
+import neutron_plugin
+
+
+class TestRBACPolicy(unittest.TestCase):
+    test_node_instance_id = 'test-rbac-policy-instance-id'
+    test_tenant_id = '11111111111111111111'
+    test_os_rbac_policy_id = '222222222222222'
+    test_os_network_id = '333333333333333'
+    test_deployment_id = 'test-deployment-id'
+
+    class MockRBACPolicyOS:
+        def __init__(self,
+                     id,
+                     action,
+                     object_id,
+                     object_type='network',
+                     target_tenant='*'):
+            self._id = id
+            self._action = action
+            self._object_id = object_id
+            self._object_type = object_type
+            self._target_tenant = target_tenant
+
+        @property
+        def id(self):
+            return self._id
+
+        @property
+        def action(self):
+            return self._action
+
+        @property
+        def object_id(self):
+            return self._object_id
+
+        @property
+        def object_type(self):
+            return self._object_type
+
+        @property
+        def target_tenant(self):
+            return self._target_tenant
+
+        def to_dict(self):
+            return dict(
+                [(k.strip('_'), v) for k, v in vars(self).iteritems()]
+            )
+
+    def mock_neutron_client(self, mock_rbac_policy):
+        neutron_client = mock.MagicMock()
+
+        neutron_client.cosmo_get_if_exists.return_value = mock_rbac_policy
+        neutron_client.get_name_from_resource.return_value = None
+        neutron_client.get_id_from_resource.return_value = \
+            self.test_os_rbac_policy_id
+        neutron_client.cosmo_delete_resource = mock.MagicMock()
+        neutron_client.create_rbac_policy.return_value = {
+            'rbac_policy': mock_rbac_policy.to_dict()
+        }
+        neutron_client.show_rbac_policy.return_value = mock_rbac_policy
+        neutron_client.list_rbac_policies.return_value = {
+            'rbac_policies': [mock_rbac_policy.to_dict()]
+        }
+
+        return neutron_client
+
+    def mock_ctx(self,
+                 test_properties,
+                 test_node_instance_id,
+                 test_deployment_id,
+                 runtime_properties=None,
+                 test_relationships=None):
+
+        ctx = MockContext()
+        ctx.node = MockNodeContext(properties=test_properties)
+        ctx.instance = MockNodeInstanceContext(
+            id=test_node_instance_id,
+            runtime_properties=runtime_properties or {},
+            relationships=test_relationships or []
+        )
+        ctx.deployment = mock.Mock()
+        ctx.deployment.id = test_deployment_id
+        ctx.bootstrap_context = mock.Mock()
+        setattr(ctx.bootstrap_context, 'resources_prefix', '')
+        ctx.type = NODE_INSTANCE
+        ctx.logger = mock.Mock()
+
+        return ctx
+
+    def mock_properties(self,
+                        use_external_resource=False,
+                        create_if_missing=False,
+                        resource_id='',
+                        include_reference=True):
+        rbac_properties = {
+            'target_tenant': self.test_tenant_id,
+            'action': 'access_as_shared'
+        }
+
+        if include_reference:
+            rbac_properties['object_type'] = 'network'
+            rbac_properties['object_id'] = self.test_os_network_id
+
+        properties = {
+            'resource_id': resource_id,
+            'use_external_resource': use_external_resource,
+            'create_if_missing': create_if_missing,
+            RBAC_POLICY_OPENSTACK_TYPE: rbac_properties
+        }
+
+        return properties
+
+    def mock_relationship(self,
+                          type=RBAC_POLICY_APPLIED_FOR_RELATIONSHIP_TYPE,
+                          runtime_properties=None):
+
+        class _MockRelationshipContext(MockRelationshipContext):
+
+            @property
+            def type_hierarchy(self):
+                return [self.type]
+
+        return _MockRelationshipContext(
+            MockRelationshipSubjectContext(
+                node=None,
+                instance=MockNodeInstanceContext(
+                    runtime_properties=runtime_properties or {},
+                )
+            ),
+            type=type
+        )
+
+    def mock_all(self, relationships=None, **kwargs):
+        ctx = self.mock_ctx(
+            self.mock_properties(**kwargs),
+            self.test_node_instance_id,
+            self.test_deployment_id,
+            {
+                OPENSTACK_ID_PROPERTY: self.test_node_instance_id,
+                OPENSTACK_NAME_PROPERTY: None,
+                OPENSTACK_TYPE_PROPERTY: RBAC_POLICY_OPENSTACK_TYPE,
+            },
+            relationships or []
+        )
+        neutron_plugin.rbac_policy.ctx = ctx
+        mocked_rbac_policy = self.MockRBACPolicyOS(
+            id=self.test_os_rbac_policy_id,
+            object_id=self.test_os_network_id,
+            action='access_as_shared',
+            target_tenant=self.test_tenant_id
+        )
+        neutron_client = self.mock_neutron_client(mocked_rbac_policy)
+
+        return ctx, neutron_client, mocked_rbac_policy
+
+    @mock.patch(
+        'openstack_plugin_common._handle_kw',
+        autospec=True,
+        return_value=None
+    )
+    def test_create_and_delete(self, *_):
+        # given
+        ctx, neutron_client, _ = self.mock_all()
+
+        # when (create)
+        neutron_plugin.rbac_policy.create(neutron_client, {})
+
+        # then (create)
+        neutron_client.create_rbac_policy.assert_called_once()
+
+        self.assertEqual(
+            None,
+            ctx.instance.runtime_properties[OPENSTACK_NAME_PROPERTY]
+        )
+        self.assertEqual(
+            self.test_os_rbac_policy_id,
+            ctx.instance.runtime_properties[OPENSTACK_ID_PROPERTY]
+        )
+        self.assertEqual(
+            RBAC_POLICY_OPENSTACK_TYPE,
+            ctx.instance.runtime_properties[OPENSTACK_TYPE_PROPERTY]
+        )
+
+        # when (delete)
+        neutron_plugin.rbac_policy.delete(neutron_client)
+
+        # then (delete)
+        neutron_client.cosmo_delete_resource.assert_called_once_with(
+            RBAC_POLICY_OPENSTACK_TYPE,
+            self.test_os_rbac_policy_id
+        )
+        self.assertNotIn(
+            OPENSTACK_ID_PROPERTY,
+            ctx.instance.runtime_properties
+        )
+        self.assertNotIn(
+            OPENSTACK_NAME_PROPERTY,
+            ctx.instance.runtime_properties
+        )
+        self.assertNotIn(
+            OPENSTACK_TYPE_PROPERTY,
+            ctx.instance.runtime_properties
+        )
+
+    @mock.patch(
+        'openstack_plugin_common._handle_kw',
+        autospec=True,
+        return_value=None
+    )
+    def test_create_and_delete_external_resource(self, *_):
+        # given
+        ctx, neutron_client, _ = self.mock_all(use_external_resource=True)
+
+        # when (create)
+        neutron_plugin.rbac_policy.create(neutron_client, {})
+
+        # then (create)
+        neutron_client.create_rbac_policy.assert_not_called()
+
+        self.assertEqual(
+            None,
+            ctx.instance.runtime_properties[OPENSTACK_NAME_PROPERTY]
+        )
+        self.assertEqual(
+            self.test_os_rbac_policy_id,
+            ctx.instance.runtime_properties[OPENSTACK_ID_PROPERTY]
+        )
+        self.assertEqual(
+            RBAC_POLICY_OPENSTACK_TYPE,
+            ctx.instance.runtime_properties[OPENSTACK_TYPE_PROPERTY]
+        )
+
+        # when (delete)
+        neutron_plugin.rbac_policy.delete(neutron_client)
+
+        # then (delete)
+        neutron_client.cosmo_delete_resource.assert_not_called()
+
+        self.assertNotIn(
+            OPENSTACK_ID_PROPERTY,
+            ctx.instance.runtime_properties
+        )
+        self.assertNotIn(
+            OPENSTACK_NAME_PROPERTY,
+            ctx.instance.runtime_properties
+        )
+        self.assertNotIn(
+            OPENSTACK_TYPE_PROPERTY,
+            ctx.instance.runtime_properties
+        )
+
+    @mock.patch(
+        'openstack_plugin_common._handle_kw',
+        autospec=True,
+        return_value=None
+    )
+    def test_create_and_delete_using_relationship(self, *_):
+        # given
+        ctx, neutron_client, _ = self.mock_all(
+            include_reference=False,
+            relationships=[
+                self.mock_relationship(
+                    runtime_properties={
+                        OPENSTACK_TYPE_PROPERTY: 'network',
+                        OPENSTACK_ID_PROPERTY: self.test_os_network_id
+                    }
+                ),
+                self.mock_relationship(
+                    type='cloudify.relationships.depends_on'
+                )
+            ]
+        )
+
+        # when (create)
+        neutron_plugin.rbac_policy.create(neutron_client, {})
+
+        # then (create)
+        neutron_client.create_rbac_policy.assert_called_once()
+
+        self.assertEqual(
+            None,
+            ctx.instance.runtime_properties[OPENSTACK_NAME_PROPERTY]
+        )
+        self.assertEqual(
+            self.test_os_rbac_policy_id,
+            ctx.instance.runtime_properties[OPENSTACK_ID_PROPERTY]
+        )
+        self.assertEqual(
+            RBAC_POLICY_OPENSTACK_TYPE,
+            ctx.instance.runtime_properties[OPENSTACK_TYPE_PROPERTY]
+        )
+
+        # when (delete)
+        neutron_plugin.rbac_policy.delete(neutron_client)
+
+        # then (delete)
+        neutron_client.cosmo_delete_resource.assert_called_once_with(
+            RBAC_POLICY_OPENSTACK_TYPE,
+            self.test_os_rbac_policy_id
+        )
+        self.assertNotIn(
+            OPENSTACK_ID_PROPERTY,
+            ctx.instance.runtime_properties
+        )
+        self.assertNotIn(
+            OPENSTACK_NAME_PROPERTY,
+            ctx.instance.runtime_properties
+        )
+        self.assertNotIn(
+            OPENSTACK_TYPE_PROPERTY,
+            ctx.instance.runtime_properties
+        )
+
+    @mock.patch(
+        'openstack_plugin_common._handle_kw',
+        autospec=True,
+        return_value=None
+    )
+    def test_fail_create_using_multiple_relationships(self, *_):
+        # given
+        ctx, neutron_client, _ = self.mock_all(
+            include_reference=False,
+            relationships=[
+                self.mock_relationship(
+                    runtime_properties={
+                        OPENSTACK_TYPE_PROPERTY: 'network',
+                        OPENSTACK_ID_PROPERTY: self.test_os_network_id
+                    }
+                ),
+                self.mock_relationship()
+            ]
+        )
+
+        # when + then
+        with self.assertRaises(NonRecoverableError):
+            neutron_plugin.rbac_policy.create(neutron_client, {})
+
+    @mock.patch(
+        'openstack_plugin_common._handle_kw',
+        autospec=True,
+        return_value=None
+    )
+    def test_fail_create_using_relationship_with_missing_data(self, *_):
+        # given
+        ctx, neutron_client, _ = self.mock_all(
+            include_reference=False,
+            relationships=[self.mock_relationship()]
+        )
+
+        # when
+        neutron_plugin.rbac_policy.create(neutron_client, {})
+
+        # then
+        neutron_client.create_rbac_policy.assert_called_once_with({
+            'rbac_policy': {
+                'target_tenant': self.test_tenant_id,
+                'action': 'access_as_shared'
+            }
+        })
+        # should cause "Bad Request" openstack API error bacause of lack
+        # "object_id" and "object_type" fields
+
+    @mock.patch(
+        'openstack_plugin_common._handle_kw',
+        autospec=True,
+        return_value=None
+    )
+    def test_fail_create_using_relationship_and_properties(self, *_):
+        # given
+        ctx, neutron_client, _ = self.mock_all(
+            relationships=[
+                self.mock_relationship(
+                    runtime_properties={
+                        OPENSTACK_TYPE_PROPERTY: 'network',
+                        OPENSTACK_ID_PROPERTY: self.test_os_network_id
+                    }
+                )
+            ]
+        )
+
+        # when + then
+        with self.assertRaises(NonRecoverableError):
+            neutron_plugin.rbac_policy.create(neutron_client, {})
+
+    @mock.patch(
+        'openstack_plugin_common._handle_kw',
+        autospec=True,
+        return_value=None
+    )
+    def test_create_and_delete_using_args(self, *_):
+        # given
+        ctx, neutron_client, _ = self.mock_all(
+            include_reference=False,
+        )
+
+        # when (create)
+        neutron_plugin.rbac_policy.create(
+            neutron_client,
+            self.mock_properties()
+        )
+
+        # then (create)
+        neutron_client.create_rbac_policy.assert_called_once()
+
+        self.assertEqual(
+            None,
+            ctx.instance.runtime_properties[OPENSTACK_NAME_PROPERTY]
+        )
+        self.assertEqual(
+            self.test_os_rbac_policy_id,
+            ctx.instance.runtime_properties[OPENSTACK_ID_PROPERTY]
+        )
+        self.assertEqual(
+            RBAC_POLICY_OPENSTACK_TYPE,
+            ctx.instance.runtime_properties[OPENSTACK_TYPE_PROPERTY]
+        )
+
+        # when (delete)
+        neutron_plugin.rbac_policy.delete(neutron_client)
+
+        # then (delete)
+        neutron_client.cosmo_delete_resource.assert_called_once_with(
+            RBAC_POLICY_OPENSTACK_TYPE,
+            self.test_os_rbac_policy_id
+        )
+        self.assertNotIn(
+            OPENSTACK_ID_PROPERTY,
+            ctx.instance.runtime_properties
+        )
+        self.assertNotIn(
+            OPENSTACK_NAME_PROPERTY,
+            ctx.instance.runtime_properties
+        )
+        self.assertNotIn(
+            OPENSTACK_TYPE_PROPERTY,
+            ctx.instance.runtime_properties
+        )
+
+    @mock.patch(
+        'openstack_plugin_common._handle_kw',
+        autospec=True,
+        return_value=None
+    )
+    def test_fail_create_using_relationship_and_args(self, *_):
+        # given
+        ctx, neutron_client, _ = self.mock_all(
+            include_reference=False,
+            relationships=[
+                self.mock_relationship(
+                    runtime_properties={
+                        OPENSTACK_TYPE_PROPERTY: 'network',
+                        OPENSTACK_ID_PROPERTY: self.test_os_network_id
+                    }
+                )
+            ]
+        )
+
+        # when + then
+        with self.assertRaises(NonRecoverableError):
+            neutron_plugin.rbac_policy.create(
+                neutron_client,
+                self.mock_properties()
+            )
+
+    @mock.patch(
+        'openstack_plugin_common._handle_kw',
+        autospec=True,
+        return_value=None
+    )
+    def test_list(self, *_):
+        # given
+        ctx, neutron_client, _ = self.mock_all()
+
+        # when
+        neutron_plugin.rbac_policy.list_rbac_policies(neutron_client, {})
+
+        # then
+        rbac_policies_list_key = '{}_list'.format(RBAC_POLICY_OPENSTACK_TYPE)
+        self.assertIn(rbac_policies_list_key, ctx.instance.runtime_properties)
+        self.assertEqual(
+            [{
+                'target_tenant': self.test_tenant_id,
+                'action': 'access_as_shared',
+                'object_type': 'network',
+                'object_id': self.test_os_network_id,
+                'id': self.test_os_rbac_policy_id
+            }],
+            ctx.instance.runtime_properties[rbac_policies_list_key]
+        )
+
+    @mock.patch(
+        'openstack_plugin_common._handle_kw',
+        autospec=True,
+        return_value=None
+    )
+    def test_find_and_delete(self, *_):
+        # given
+        ctx, neutron_client, _ = self.mock_all()
+
+        # when
+        neutron_plugin.rbac_policy.find_and_delete(neutron_client, {})
+
+        # then
+        neutron_client.list_rbac_policies.assert_called_once()
+        neutron_client.cosmo_delete_resource.assert_called_once_with(
+            RBAC_POLICY_OPENSTACK_TYPE,
+            self.test_os_rbac_policy_id
+        )

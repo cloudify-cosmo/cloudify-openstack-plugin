ad8e31452b0f7127bb656dcdc2723fd30a008756
Mohammed AbuAisha <mohammeda@cloudify.co>
2018-10-10 16:39:20 +0300

    update versions and changelog

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 setup(
     zip_safe=True,
     name='cloudify-openstack-plugin',
-    version='2.12.0',
+    version='2.12.1',
     author='Cloudify',
     author_email='hello@cloudify.co',
     packages=[

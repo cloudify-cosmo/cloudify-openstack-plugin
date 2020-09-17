390bd2266523afe13f67bf738d50d52f0eb45f9c
EarthmanT <EarthmanT@users.noreply.github.com>
2020-09-17 15:15:29 +0300

    check correct merge branch (#373)
    __NODOCS__

--- .circleci/merge_docs.py
+++ .circleci/merge_docs.py
@@ -1,4 +1,4 @@
 from ecosystem_cicd_tools.github_stuff import merge_documentation_pulls
 
 if __name__ == '__main__':
-    merge_documentation_pulls()
+    merge_documentation_pulls(branch='2.14.21-build')

f3fdcfbef126ca59b3d5d17dd14d57a6f294e7ea
EarthmanT <EarthmanT@users.noreply.github.com>
2023-10-01 12:19:27 -0400

    bump version

--- setup.py
+++ setup.py
@@ -15,25 +15,18 @@
 
 
 import os
+import re
+import pathlib
 from setuptools import setup
-from setuptools import find_packages
 
 
-def read(rel_path):
-    here = os.path.abspath(os.path.dirname(__file__))
-    with open(os.path.join(here, rel_path), 'r') as fp:
-        return fp.read()
-
-
-def get_version(rel_file='plugin.yaml'):
-    lines = read(rel_file)
-    for line in lines.splitlines():
-        if 'package_version' in line:
-            split_line = line.split(':')
-            line_no_space = split_line[-1].replace(' ', '')
-            line_no_quotes = line_no_space.replace('\'', '')
-            return line_no_quotes.strip('\n')
-    raise RuntimeError('Unable to find version string.')
+def get_version():
+    current_dir = pathlib.Path(__file__).parent.resolve()
+    with open(os.path.join(current_dir,
+                           'openstack_plugin/__version__.py'),
+              'r') as outfile:
+        var = outfile.read()
+        return re.search(r'\d+.\d+.\d+', var).group()
 
 
 setup(
@@ -45,7 +38,7 @@ setup(
     zip_safe=False,
     packages=find_packages(exclude=['tests*']),
     install_requires=[
-        'cloudify-common>=4.5.5',
+        'cloudify-common>=7.0.0',
         'openstacksdk==0.53.0',
         'IPy==0.81',
         'pycryptodome>=3.9.8,<3.10',

a674c9839077dd429c0407fd65e63dfaccb32ab1
EarthmanT <EarthmanT@users.noreply.github.com>
2023-10-01 16:49:37 -0400

    3.3.6 build (#433)
    * update for build
    
    * bump version
    
    * remove executor
    
    * foo
    
    * making some changes

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-########
-# Copyright (c) 2018 Cloudify Platform Ltd. All rights reserved
+# Copyright (c) 2017-2019 Cloudify Platform Ltd. All rights reserved
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -9,31 +8,23 @@
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-#    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    * See the License for the specific language governing permissions and
-#    * limitations under the License.
-
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import os
-from setuptools import setup
-from setuptools import find_packages
+import re
+import pathlib
+from setuptools import setup, find_packages
 
 
-def read(rel_path):
-    here = os.path.abspath(os.path.dirname(__file__))
-    with open(os.path.join(here, rel_path), 'r') as fp:
-        return fp.read()
-
-
-def get_version(rel_file='plugin.yaml'):
-    lines = read(rel_file)
-    for line in lines.splitlines():
-        if 'package_version' in line:
-            split_line = line.split(':')
-            line_no_space = split_line[-1].replace(' ', '')
-            line_no_quotes = line_no_space.replace('\'', '')
-            return line_no_quotes.strip('\n')
-    raise RuntimeError('Unable to find version string.')
+def get_version():
+    current_dir = pathlib.Path(__file__).parent.resolve()
+    with open(os.path.join(current_dir,
+                           'openstack_plugin/__version__.py'),
+              'r') as outfile:
+        var = outfile.read()
+        return re.search(r'\d+.\d+.\d+', var).group()
 
 
 setup(

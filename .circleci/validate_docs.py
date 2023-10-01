04a0172082301c1201f7935b51ebe73a6c101f5a
EarthmanT <EarthmanT@users.noreply.github.com>
2023-10-01 14:57:17 -0400

    foo

--- .circleci/validate_docs.py
+++
@@ -1,4 +0,0 @@
-from ecosystem_cicd_tools.validations import validate_documentation_pulls
-
-if __name__ == '__main__':
-    validate_documentation_pulls()
--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-########
-# Copyright (c) 2018 Cloudify Platform Ltd. All rights reserved
+# Copyright (c) 2017-2019 Cloudify Platform Ltd. All rights reserved
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -9,15 +8,14 @@
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-#    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    * See the License for the specific language governing permissions and
-#    * limitations under the License.
-
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import os
 import re
 import pathlib
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 def get_version():
@@ -38,7 +36,7 @@ setup(
     zip_safe=False,
     packages=find_packages(exclude=['tests*']),
     install_requires=[
-        'cloudify-common>=7.0.0',
+        'cloudify-common>=4.5.5',
         'openstacksdk==0.53.0',
         'IPy==0.81',
         'pycryptodome>=3.9.8,<3.10',

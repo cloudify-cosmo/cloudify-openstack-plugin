737ba797d7c384569e737f6e96d1e9b56a4f4fd0
EarthmanT <EarthmanT@users.noreply.github.com>
2023-10-23 21:10:10 -0400

    bump versions

--- setup.py
+++ setup.py
@@ -14,24 +14,20 @@
 #  * limitations under the License.
 
 import os
+import re
+import pathlib
+
 from setuptools import setup
+from setuptools import find_packages
+
+VERSION_FILE = 'openstack_plugin_common/__version__.py'
 
 
-def read(rel_path):
-    here = os.path.abspath(os.path.dirname(__file__))
-    with open(os.path.join(here, rel_path), 'r') as fp:
-        return fp.read()
-
-
-def get_version(rel_file='plugin.yaml'):
-    lines = read(rel_file)
-    for line in lines.splitlines():
-        if 'package_version' in line:
-            split_line = line.split(':')
-            line_no_space = split_line[-1].replace(' ', '')
-            line_no_quotes = line_no_space.replace('\'', '')
-            return line_no_quotes.strip('\n')
-    raise RuntimeError('Unable to find version string.')
+def get_version():
+    current_dir = pathlib.Path(__file__).parent.resolve()
+    with open(os.path.join(current_dir, VERSION_FILE), 'r') as outfile:
+        var = outfile.read()
+        return re.search(r'\d+.\d+.\d+', var).group()
 
 
 setup(
@@ -51,7 +47,7 @@ setup(
     license='LICENSE',
     description='Cloudify plugin for OpenStack infrastructure.',
     install_requires=[
-        'cloudify-common>=4.6',
+        'cloudify-common>=4.6,<7.0.0',
         'keystoneauth1>=2.16.0,<3',
         'python-novaclient==7.0.0',
         'python-keystoneclient==3.5.0',

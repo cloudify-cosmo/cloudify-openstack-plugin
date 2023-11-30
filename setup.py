b4fbecec48ca61d9fd5ccf096be3ecdfcdf92432
EarthmanT <EarthmanT@users.noreply.github.com>
2023-11-30 12:21:01 -0500

    update requirements.txt (#436)

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@
 import os
 import re
 import pathlib
+import sys
 from setuptools import setup, find_packages
 
 
@@ -27,6 +28,40 @@ def get_version():
         return re.search(r'\d+.\d+.\d+', var).group()
 
 
+install_requires = [
+    'openstacksdk==0.53.0',
+    'IPy==0.81',
+    'pycryptodome>=3.9.8,<3.10',
+    'python-manilaclient==2.0.0'
+]
+
+if sys.version_info.major == 3 and sys.version_info.minor == 6:
+    install_requires += [
+        'cloudify-common>=4.5,<7.0',
+        'cloudify-utilities-plugins-sdk>=0.0.91',  # includes YAML
+    ]
+    packages = [
+        'openstack_plugin',
+        'openstack_sdk',
+        'openstack_plugin.resources',
+        'openstack_plugin.resources.compute',
+        'openstack_plugin.resources.dns_service',
+        'openstack_plugin.resources.identity',
+        'openstack_plugin.resources.network',
+        'openstack_plugin.resources.share',
+        'openstack_plugin.resources.volume',
+        'openstack_plugin.tests.dns_service',
+        'openstack_sdk.resources',
+    ]
+else:
+    install_requires += [
+        'fusion-common',
+        'fusion-mgmtworker',
+        'cloudify-utilities-plugins-sdk',
+    ]
+    packages = find_packages(exclude=['tests*'])
+
+
 setup(
     name='cloudify-openstack-plugin',
     version=get_version(),
@@ -34,16 +69,6 @@ setup(
     author_email='info@cloudify.co',
     license='LICENSE',
     zip_safe=False,
-    packages=find_packages(exclude=['tests*']),
-    install_requires=[
-        'cloudify-common>=4.5.5',
-        'openstacksdk==0.53.0',
-        'IPy==0.81',
-        'pycryptodome>=3.9.8,<3.10',
-        'python-manilaclient==2.0.0'
-    ],
-    test_requires=[
-        'mock',
-        'requests-mock'
-    ]
+    packages=packages,
+    install_requires=install_requires,
 )
